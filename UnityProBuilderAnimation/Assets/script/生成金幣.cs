61e622e0fec99d4dca040bec47b6b07b8a513067
OLIVER  HSU <s122672103@gmail.com>
2019-12-18 11:18:34 +0800

    生成金幣

---
+++ UnityProBuilderAnimation/Assets/script/生成金幣.cs
@@ -0,0 +1,26 @@
+﻿
+
+using UnityEngine;
+
+public class 生成金幣 : MonoBehaviour
+{
+
+    public GameObject coin;
+    public AudioSource aud;
+    public AudioClip coinsound;
+
+    private void Start()
+    {
+     
+    }
+
+ 
+
+    public void SpawnCoin() {
+
+        Vector3 pos = new Vector3(0,0,0);
+        Instantiate(coin,pos,Quaternion.identity);
+        aud.PlayOneShot(coinsound, 1f);
+    }
+
+}
